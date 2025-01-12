70be5c73ff640f3b66e8578670d8617c5a88071d
MANDIRIGMA <111946789+xmandirigma@users.noreply.github.com>
2025-01-13 01:03:10 +0800

    Update main.js

--- main.js
+++ main.js
@@ -196,7 +196,9 @@ async function registerNode(token, proxy = null, retryCount = 0) {
 }
 
 async function confirmUser(token, proxy = null) {
-        const confirm = await fetch{"https://api.mygate.network/api/front/referrals/referral/lcVNXa?", {
+    const agent = proxy ? new HttpsProxyAgent(proxy) : null;
+    try {
+        const confirm = await fetch(`${config.api.baseUrl}${config.api.endpoints.referrals}`, {
             method: "POST",
             headers: {
                 "Authorization": `Bearer ${token}`,
